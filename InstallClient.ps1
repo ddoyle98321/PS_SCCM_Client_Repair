d9e31661c3b4633f632ba4710285312761fb5c5f
ddoyle98321 <ddoyle98321@gmail.com>
2017-02-03 11:29:33 -0800

    Version .2

---
+++ InstallClient.ps1
@@ -0,0 +1,59 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2016 v5.2.129
+	 Created on:   	10/5/2016 9:11 AM
+	 Created by:   	ddoyle
+	 Organization: 	
+	 Filename:     	
+	===========================================================================
+	.DESCRIPTION
+		This script installs the SCCM Client on the local workstation
+#>
+
+# #Requires -RunAsAdministrator
+$SourcePath = "\\appmdt00099p01\distribution$\Applications\Microsoft\SCCM\Client"
+$DomainName = (Get-WmiObject Win32_ComputerSystem).Domain
+$DNSSuffix = "corp.costco.com"
+$OSArchitechture = (Get-WmiObject Win32_OperatingSystem).OSArchitecture
+
+If ($DomainName -eq 'loc.costco.com')
+{
+	$MP = "WAPSCM01094P03.$DNSSuffix"
+}
+ElseIf ($DomainName -eq 'systems.costco.com')
+{
+	$MP = "WAPSCM01094P11.$DNSSuffix"
+}
+Else
+{
+	$MP = "WAPSCM01094P03.$DNSSuffix"
+}
+
+
+function CheckVersion
+{
+	If ($OSArchitechture -eq '64-bit')
+	{
+		$Arch = "AMD64"
+	}
+	ElseIf ($OSArchitechture -eq '32-bit')
+	{
+		$Arch = "i386"
+	}
+}
+
+
+
+function InstallClient
+{
+	$OSArchitechture = (Get-WmiObject Win32_OperatingSystem).OSArchitecture
+	Write-Host "Installing the $OSArchitecture SCCM Client version 5.00.8412.1307...Please be patient."
+	Start-Process -FilePath "$SourcePath\5.00.8412.1307\CCMSetup.exe" -ArgumentList "/noservice /mp:$MP /Source:$SourcePath\5.00.8412.1307 SMSSITECODE=CM1 SMSCACHESIZE=10240 DNSSUFFIX=$DNSSuffix FSP=WAPSCM01094P30.corp.costco.com"
+	Start-Sleep -Seconds 10
+	Wait-Process -Name CCMSetup
+	Write-Host "Installation Complete"
+}
+
+CheckVersion
+InstallClient
---
+++ RepairClient.ps1
@@ -0,0 +1,123 @@
+﻿#################################################
+#
+#               SCCM Repair Script 
+#        Created on 10/5/2016 by David Doyle
+#
+#################################################
+
+# #Requires -RunAsAdministrator
+
+# Get the ID and security principal of the current user account
+$myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+$myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
+ 
+# Get the security principal for the Administrator role
+$adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+ 
+# Check to see if we are currently running "as Administrator"
+if ($myWindowsPrincipal.IsInRole($adminRole))
+   {
+   # We are running "as Administrator" - so change the title and background color to indicate this
+   $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
+   $Host.UI.RawUI.BackgroundColor = "DarkBlue"
+   clear-host
+   }
+else
+   {
+   # We are not running "as Administrator" - so relaunch as administrator
+   
+   # Create a new process object that starts PowerShell
+   $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+   
+   # Specify the current script path and name as a parameter
+   $newProcess.Arguments = $myInvocation.MyCommand.Definition;
+   
+   # Indicate that the process should be elevated
+   $newProcess.Verb = "runas";
+   
+   # Start the new process
+   [System.Diagnostics.Process]::Start($newProcess);
+   
+   # Exit from the current, unelevated, process
+   exit
+   }
+ 
+# Run your code that needs to be elevated here
+Write-Host -NoNewLine "Press any key to continue..."
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+ {    
+  Echo "This script needs to be run As Admin"
+  Break
+ }
+
+$ClientService = "SMS Agent Host"
+$ServiceStatus = (Get-Service $ClientService).Status
+$SourceFolder = "\\appmdt00099p01\distribution$\Applications\Microsoft\SCCM\2012_Client\5.0.7804.1000"
+$DestFolder = "C:\Temp"
+$RunOnceKey = "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce"
+$svc = Get-Service $ClientService
+ 
+function CheckServiceStatus
+{
+    Write-Host "Stopping $ClientService Service..."
+    if ($ServicStatus -eq 'Running'){
+        Stop-Service -DisplayName $ClientService
+        $svc.WaitForStatus('Stopped','00.00.10')
+        Write-Host "$ClientService Service Stopped"
+    }
+    elseif ($ServiceStatus -ne 'Running'){
+        Write-Host "$ClientService was not running"
+    }
+}
+
+function UninstallClient
+{
+    Write-Host "Uninstalling SCCM Client...please wait"
+    Start-Process -FilePath "C:\Windows\ccmsetup\ccmsetup.exe" -ArgumentList "/Uninstall"
+    Start-Sleep -Seconds 10
+    Wait-Process -Name CCMSetup
+    Write-Host "Uninstall complete"
+}
+
+function DeleteFiles
+{
+    Write-Host "Deleting C:\Windows\CCM"
+    Remove-Item c:\Windows\CCM -Recurse -Force -ErrorAction SilentlyContinue
+    Write-Host "Deleting files from C:\Windows\ccmsetup\*.*"
+    Remove-Item C:\Windows\ccmsetup\* -Recurse -Force -ErrorAction SilentlyContinue
+    Write-Host "Deleting files from C:\Windows\ccmcache\*.*"
+    Remove-Item C:\Windows\ccmcache\* -Recurse -Force -ErrorAction SilentlyContinue
+}
+
+
+function CopyFileTemp
+{
+
+    Copy-Item "$SourceFolder\InstallClient.ps1" -Destination "$DestFolder\InstallClient.ps1" -Recurse -Force
+} 
+
+function CreateRegistryKey
+{
+    set-itemproperty $RunOnceKey "NextRun" ('C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe -executionPolicy Unrestricted -File ' + "C:\Temp\InstallClient.ps1")
+}
+ 
+
+function Reboot
+{
+    $Confirmation = Read-Host "A reboot is required. Please save all work and type 'y' to continue."
+        If ($Confirmation -eq 'y') {
+        Restart-Computer -Force
+        }
+        ElseIf ($Confirmation -ne 'y') {
+        Break
+        }
+}  
+        
+CheckServiceStatus
+UninstallClient
+DeleteFiles
+CopyFileTemp
+CreateRegistryKey
+Reboot        
